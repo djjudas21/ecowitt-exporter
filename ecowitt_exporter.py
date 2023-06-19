d4eb3b6ea3c4cfdc81de6fcef6f2055b09bce3bb
Jonathan Gazeley <me@jonathangazeley.com>
2023-06-19 16:08:26 +0100

    Add Prometheus gauges and cater for metric and imperial

--- ecowitt_exporter.py
+++ ecowitt_exporter.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
+from prometheus_client import make_wsgi_app, Gauge
 import os
 
 app = Flask(__name__)
@@ -32,36 +32,94 @@ def logEcowitt():
         # Convert degrees Fahrenheit to Celsius
         if key in ['tempinf', 'tempf', 'temp1f', 'temp2f', 'temp3f', 'temp4f', 'temp5f', 'temp6f', 'temp7f', 'temp8f']:
             tempC = (float(value) - 32) * 5/9
-            value = "{:.2f}".format(tempC)
-            key = key[:-1] + 'c'
+            valueC = "{:.2f}".format(tempC)
+            keyC = key[:-1] + 'c'
+            temperature[key].set(value)
+            temperature[keyC].set(valueC)
 
         # Convert pressure inches to hPa
         if key in ['baromrelin', 'baromabsin']:
             pressureHpa = float(value) * 33.6585
-            value = "{:.2f}".format(pressureHpa)
-            key = key[:-2] + 'hpa'
+            valueHpa = "{:.2f}".format(pressureHpa)
+            keyHpa = key[:-2] + 'hpa'
+            pressure[key].set(value)
+            pressure[keyHpa].set(valueHpa)
 
         # Convert speed mph to km/h
         if key in ['windspeedmph', 'windgustmph', 'maxdailygust']:
-            speed = float(value) * 1.60934
-            value = "{:.2f}".format(speed)
+            speedkmh = float(value) * 1.60934
+            valuekmh = "{:.2f}".format(speedkmh)
             if key == 'maxdailygust':
-                key = key + 'kmh'
+                keykmh = key + 'kmh'
             else:
-                key = key[:-3] + 'kmh'
+                keykmh = key[:-3] + 'kmh'
+            wind[key].set(value)
+            wind[keykmh].set(valuekmh)
 
         # Convert rain inches to mm
         if key in ['rainratein', 'eventrainin', 'hourlyrainin', 'dailyrainin', 'weeklyrainin', 'monthlyrainin', 'yearlyrainin', 'totalrainin']:
             mm = float(value) * 25.4
-            value = "{:.1f}".format(mm)
-            key = key[:-2] + 'mm'
-
-        if fields:
-            fields += "," + key + "=" + value
-        else:
-            fields = key + "=" + value
+            valuemm = "{:.1f}".format(mm)
+            keymm = key[:-2] + 'mm'
+            rain[key].set(value)
+            rain[keymm].set(valuemm)
 
 if __name__ == "__main__":
+    # Set up various Prometheus metrics with descriptions and units
+    temperature={}
+    temperature['tempinf'] = Gauge(name='tempinf', documentation='temps', unit='f')
+    temperature['tempf'] = Gauge(name='tempf', documentation='tempf', unit='f')
+    temperature['temp1f'] = Gauge(name='temp1f', documentation='temp1f', unit='f')
+    temperature['temp2f'] = Gauge(name='temp2f', documentation='temp2f', unit='f')
+    temperature['temp3f'] = Gauge(name='temp3f', documentation='temp3f', unit='f')
+    temperature['temp4f'] = Gauge(name='temp4f', documentation='temp4f', unit='f')
+    temperature['temp5f'] = Gauge(name='temp5f', documentation='temp5f', unit='f')
+    temperature['temp6f'] = Gauge(name='temp6f', documentation='temp6f', unit='f')
+    temperature['temp7f'] = Gauge(name='temp7f', documentation='temp7f', unit='f')
+    temperature['temp8f'] = Gauge(name='temp8f', documentation='temp8f', unit='f')
+    temperature['tempinc'] = Gauge(name='tempinc', documentation='temps', unit='c')
+    temperature['tempc'] = Gauge(name='tempc', documentation='tempf', unit='c')
+    temperature['temp1c'] = Gauge(name='temp1c', documentation='temp1f', unit='c')
+    temperature['temp2c'] = Gauge(name='temp2c', documentation='temp2f', unit='c')
+    temperature['temp3c'] = Gauge(name='temp3c', documentation='temp3f', unit='c')
+    temperature['temp4c'] = Gauge(name='temp4c', documentation='temp4f', unit='c')
+    temperature['temp5c'] = Gauge(name='temp5c', documentation='temp5f', unit='c')
+    temperature['temp6c'] = Gauge(name='temp6c', documentation='temp6f', unit='c')
+    temperature['temp7c'] = Gauge(name='temp7c', documentation='temp7f', unit='c')
+    temperature['temp8c'] = Gauge(name='temp8c', documentation='temp8f', unit='c')
+
+    pressure={}
+    pressure['baromrelin'] = Gauge(name='baromrelin', documentation='baromrelin', unit='in')
+    pressure['baromabsin'] = Gauge(name='baromabsin', documentation='baromabsin', unit='in')
+    pressure['baromrelhpa'] = Gauge(name='baromrelhpa', documentation='baromrelhpa', unit='hpa')
+    pressure['baromabshpa'] = Gauge(name='baromabshpa', documentation='baromabshpa', unit='hpa')
+
+    wind={}
+    wind['windspeedmph'] = Gauge(name='windspeedmph', documentation='windspeedmph', unit='mph')
+    wind['windgustmph'] = Gauge(name='windgustmph', documentation='windgustmph', unit='mph')
+    wind['maxdailygustmph'] = Gauge(name='maxdailygustmph', documentation='maxdailygustmph', unit='mph')
+    wind['windspeedkmh'] = Gauge(name='windspeedkmh', documentation='windspeedkmh', unit='kmh')
+    wind['windgustkmh'] = Gauge(name='windgustkmh', documentation='windgustkmh', unit='kmh')
+    wind['maxdailygustkmh'] = Gauge(name='maxdailygustkmh', documentation='maxdailygustkmh', unit='kmh')
+
+    rain={}
+    rain['rainratein'] = Gauge(name='rainratein', documentation='rainratein', unit='in')
+    rain['eventrainin'] = Gauge(name='eventrainin', documentation='eventrainin', unit='in')
+    rain['hourlyrainin'] = Gauge(name='hourlyrainin', documentation='hourlyrainin', unit='in')
+    rain['dailyrainin'] = Gauge(name='dailyrainin', documentation='dailyrainin', unit='in')
+    rain['weeklyrainin'] = Gauge(name='weeklyrainin', documentation='weeklyrainin', unit='in')
+    rain['monthlyrainin'] = Gauge(name='monthlyrainin', documentation='monthlyrainin', unit='in')
+    rain['yearlyrainin'] = Gauge(name='yearlyrainin', documentation='yearlyrainin', unit='in')
+    rain['totalrainin'] = Gauge(name='totalrainin', documentation='totalrainin', unit='in')
+    rain['rainratemm'] = Gauge(name='rainratemm', documentation='rainratemm', unit='mm')
+    rain['eventrainmm'] = Gauge(name='eventrainmm', documentation='eventrainmm', unit='mm')
+    rain['hourlyrainmm'] = Gauge(name='hourlyrainmm', documentation='hourlyrainmm', unit='mm')
+    rain['dailyrainmm'] = Gauge(name='dailyrainmm', documentation='dailyrainmm', unit='mm')
+    rain['weeklyrainmm'] = Gauge(name='weeklyrainmm', documentation='weeklyrainmm', unit='mm')
+    rain['monthlyrainmm'] = Gauge(name='monthlyrainmm', documentation='monthlyrainmm', unit='mm')
+    rain['yearlyrainmm'] = Gauge(name='yearlyrainmm', documentation='yearlyrainmm', unit='mm')
+    rain['totalrainmm'] = Gauge(name='totalrainmm', documentation='totalrainmm', unit='mm')
+
     # Add prometheus wsgi middleware to route /metrics requests
     app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
         '/metrics': make_wsgi_app()
