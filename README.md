4fd8e753d28c4841c4306560ce99ed1c4ddc8ae1
Jonathan Gazeley <me@jonathangazeley.com>
2023-06-20 14:25:53 +0100

    Add support for configuring units

--- README.md
+++ README.md
@@ -16,9 +16,22 @@ This exporter runs on a single HTTP port (default `8088`) and provides two endpo
 
 ## Environment variables
 
+Set the units for the export of each metric. Ecowitt weather stations always take measurements in Imperial.
+This exporter converts them on the fly if necessary, to present them to Prometheus in your desired format.
+Metric/SI is always the default. People in the USA will probably want to set everything to Imperial
+alternatives, while Brits will likely want a mixture of both!
+
+All units are expressed in lower case and without slashes, for simplicity. Apologies to scientists,
+for whom this will be a difficult time.
+
 | Variable           | Default | Choices         | Meaning                                       | Not yet supported           |
 |--------------------|---------|-----------------|-----------------------------------------------|-----------------------------|
 | `DEBUG`            | `FALSE` | `FALSE`, `TRUE` | Enable extra output for debugging             |                             |
+| `TEMPERATURE_UNIT` | `c`     | `c`, `f`        | Temperature in Celcius or Fahrenheit          |                             |
+| `PRESSURE_UNIT`    | `hpa`   | `hpa`, `in`     | Pressure in Hectopascals or inches of mercury | `mmhg`                      |
+| `WIND_UNIT`        | `kmh`   | `kmh`, `mph`    | Speed in kilometres/hour or miles/hour        | `ms`, `knots`, `fpm`, `bft` |
+| `RAIN_UNIT`        | `mm`    | `mm`, `in`      | Rainfall in millimetres or inches             |                             |
+| `IRRADIANCE_UNIT`  | `wm2`   | `wm2`           | Solar irradiance in Watts/m^2                 | `lx`, `fc`                  |
 
 ## How to configure your weather station
 
@@ -34,28 +47,64 @@ We can then view the corresponding Prometheus metrics with a simple GET request 
 
 ```
 curl http://localhost:8088/metrics
-# HELP tempinf_f temps
-# TYPE tempinf_f gauge
-tempinf_f 66.0
-# HELP tempf_f tempf
-# TYPE tempf_f gauge
-tempf_f 47.8
-# HELP temp1f_f temp1f
-# TYPE temp1f_f gauge
-temp1f_f 49.46
-# HELP temp2f_f temp2f
-# TYPE temp2f_f gauge
-temp2f_f 63.5
-# HELP tempinc_c temps
-# TYPE tempinc_c gauge
-tempinc_c 18.89
-# HELP tempc_c tempf
-# TYPE tempc_c gauge
-tempc_c 8.78
-# HELP temp1c_c temp1f
-# TYPE temp1c_c gauge
-temp1c_c 9.7
-# HELP temp2c_c temp2f
-# TYPE temp2c_c gauge
-temp2c_c 17.5
+# HELP tempin_c Indoor temperature
+# TYPE tempin_c gauge
+tempin_c 18.89
+# HELP temp_c Outdoor temperature
+# TYPE temp_c gauge
+temp_c 8.78
+# HELP humidity_percent Outdoor humidity
+# TYPE humidity_percent gauge
+humidity_percent 91.0
+# HELP humidityin_percent Indoor humidity
+# TYPE humidityin_percent gauge
+humidityin_percent 51.0
+# HELP winddir_degree Wind direction
+# TYPE winddir_degree gauge
+winddir_degree 261.0
+# HELP uv UV index
+# TYPE uv gauge
+uv 0.0
+# HELP solarradiation_wm2 Solar irradiance
+# TYPE solarradiation_wm2 gauge
+solarradiation_wm2 57.25
+# HELP baromrel_hpa Relative barometer
+# TYPE baromrel_hpa gauge
+baromrel_hpa 995.11
+# HELP baromabs_hpa Absolute barometer
+# TYPE baromabs_hpa gauge
+baromabs_hpa 995.11
+# HELP windspeed_kmh Wind speed
+# TYPE windspeed_kmh gauge
+windspeed_kmh 14.4
+# HELP windgust_kmh Wind gust
+# TYPE windgust_kmh gauge
+windgust_kmh 23.75
+# HELP maxdailygust_kmh Max daily gust
+# TYPE maxdailygust_kmh gauge
+maxdailygust_kmh 34.92
+# HELP rainrate_mm Rainfall rate
+# TYPE rainrate_mm gauge
+rainrate_mm 0.0
+# HELP eventrain_mm Event rainfall
+# TYPE eventrain_mm gauge
+eventrain_mm 1.8
+# HELP hourlyrain_mm Hourly rainfall
+# TYPE hourlyrain_mm gauge
+hourlyrain_mm 0.0
+# HELP dailyrain_mm Daily rainfall
+# TYPE dailyrain_mm gauge
+dailyrain_mm 0.3
+# HELP weeklyrain_mm Weekly rainfall
+# TYPE weeklyrain_mm gauge
+weeklyrain_mm 2.8
+# HELP monthlyrain_mm Monthly rainfall
+# TYPE monthlyrain_mm gauge
+monthlyrain_mm 36.1
+# HELP yearlyrain_mm Yearly rainfall
+# TYPE yearlyrain_mm gauge
+yearlyrain_mm 616.1
+# HELP totalrain_mm Total rainfall
+# TYPE totalrain_mm gauge
+totalrain_mm 616.1
 ```
--- ecowitt_exporter.py
+++ ecowitt_exporter.py
@@ -7,12 +7,22 @@ app = Flask(__name__)
 
 station_id = os.environ.get('STATION_ID', 'my-station')
 debug = os.environ.get('DEBUG', "FALSE")
+temperature_unit = os.environ.get('TEMPERATURE_UNIT', 'c')
+pressure_unit = os.environ.get('PRESSURE_UNIT', 'hpa')
+wind_unit = os.environ.get('WIND_UNIT', 'kmh')
+rain_unit = os.environ.get('RAIN_UNIT', 'mm')
+irradiance_unit = os.environ.get('IRRADIANCE_UNIT', 'wm2')
 
 print ("Ecowitt Exporter v0.1")
 print ("==============")
 print ("Configuration:")
-print ("  STATION_ID:      " + station_id)
-print ("  DEBUG:           " + debug)
+print ('  STATION_ID:       ' + station_id)
+print ('  DEBUG:            ' + debug)
+print ('  TEMPERATURE_UNIT: ' + temperature_unit)
+print ('  PRESSURE_UNIT:    ' + pressure_unit)
+print ('  WIND_UNIT:        ' + wind_unit)
+print ('  RAIN_UNIT:        ' + rain_unit)
+print ('  IRRADIANCE_UNIT:  ' + irradiance_unit)
 
 
 @app.route('/')
@@ -34,43 +44,45 @@ def logEcowitt():
             continue
 
         # No conversions needed
-        if key in ['humidity', 'humidityin', 'winddir', 'uv', 'solarradiation']:
+        if key in ['humidity', 'humidityin', 'winddir', 'uv']:
             generic[key].set(value)
+
+        # Solar irradiance
+        if key in ['solarradiation']:
+            irradiance[key].set(value)
 
         # Convert degrees Fahrenheit to Celsius
         if key in ['tempinf', 'tempf', 'temp1f', 'temp2f', 'temp3f', 'temp4f', 'temp5f', 'temp6f', 'temp7f', 'temp8f']:
-            tempC = (float(value) - 32) * 5/9
-            valueC = "{:.2f}".format(tempC)
-            keyC = key[:-1] + 'c'
+            if temperature_unit == 'c':
+                tempC = (float(value) - 32) * 5/9
+                value = "{:.2f}".format(tempC)
+            key = key[:-1]
             temperature[key].set(value)
-            temperature[keyC].set(valueC)
 
         # Convert pressure inches to hPa
         if key in ['baromrelin', 'baromabsin']:
-            pressureHpa = float(value) * 33.6585
-            valueHpa = "{:.2f}".format(pressureHpa)
-            keyHpa = key[:-2] + 'hpa'
+            if pressure_unit == 'hpa':
+                pressurehpa = float(value) * 33.6585
+                value = "{:.2f}".format(pressurehpa)
+            key = key[:-2]
             pressure[key].set(value)
-            pressure[keyHpa].set(valueHpa)
 
         # Convert speed mph to km/h
         if key in ['windspeedmph', 'windgustmph', 'maxdailygust']:
-            speedkmh = float(value) * 1.60934
-            valuekmh = "{:.2f}".format(speedkmh)
-            if key == 'maxdailygust':
-                keykmh = key + 'kmh'
-            else:
-                keykmh = key[:-3] + 'kmh'
+            if wind_unit == 'kmh':
+                speedkmh = float(value) * 1.60934
+                value = "{:.2f}".format(speedkmh)
+            if key != 'maxdailygust':
+                key = key[:-3]
             wind[key].set(value)
-            wind[keykmh].set(valuekmh)
 
         # Convert rain inches to mm
         if key in ['rainratein', 'eventrainin', 'hourlyrainin', 'dailyrainin', 'weeklyrainin', 'monthlyrainin', 'yearlyrainin', 'totalrainin']:
-            mm = float(value) * 25.4
-            valuemm = "{:.1f}".format(mm)
-            keymm = key[:-2] + 'mm'
+            if rain_unit == 'mm':
+                mm = float(value) * 25.4
+                value = "{:.1f}".format(mm)
+            key = key[:-2]
             rain[key].set(value)
-            rain[keymm].set(valuemm)
 
     response = app.response_class(
             response='OK',
@@ -83,65 +95,44 @@ if __name__ == "__main__":
 
     # Set up various Prometheus metrics with descriptions and units
     temperature={}
-    temperature['tempinf'] = Gauge(name='tempinf', documentation='temps', unit='f')
-    temperature['tempf'] = Gauge(name='tempf', documentation='tempf', unit='f')
-    temperature['temp1f'] = Gauge(name='temp1f', documentation='temp1f', unit='f')
-    temperature['temp2f'] = Gauge(name='temp2f', documentation='temp2f', unit='f')
-    temperature['temp3f'] = Gauge(name='temp3f', documentation='temp3f', unit='f')
-    temperature['temp4f'] = Gauge(name='temp4f', documentation='temp4f', unit='f')
-    temperature['temp5f'] = Gauge(name='temp5f', documentation='temp5f', unit='f')
-    temperature['temp6f'] = Gauge(name='temp6f', documentation='temp6f', unit='f')
-    temperature['temp7f'] = Gauge(name='temp7f', documentation='temp7f', unit='f')
-    temperature['temp8f'] = Gauge(name='temp8f', documentation='temp8f', unit='f')
-    temperature['tempinc'] = Gauge(name='tempinc', documentation='temps', unit='c')
-    temperature['tempc'] = Gauge(name='tempc', documentation='tempf', unit='c')
-    temperature['temp1c'] = Gauge(name='temp1c', documentation='temp1f', unit='c')
-    temperature['temp2c'] = Gauge(name='temp2c', documentation='temp2f', unit='c')
-    temperature['temp3c'] = Gauge(name='temp3c', documentation='temp3f', unit='c')
-    temperature['temp4c'] = Gauge(name='temp4c', documentation='temp4f', unit='c')
-    temperature['temp5c'] = Gauge(name='temp5c', documentation='temp5f', unit='c')
-    temperature['temp6c'] = Gauge(name='temp6c', documentation='temp6f', unit='c')
-    temperature['temp7c'] = Gauge(name='temp7c', documentation='temp7f', unit='c')
-    temperature['temp8c'] = Gauge(name='temp8c', documentation='temp8f', unit='c')
+    temperature['tempin'] = Gauge(name='tempin', documentation='Indoor temperature', unit=temperature_unit)
+    temperature['temp'] = Gauge(name='temp', documentation='Outdoor temperature', unit=temperature_unit)
+    temperature['temp1'] = Gauge(name='temp1', documentation='Temp 1', unit=temperature_unit)
+    temperature['temp2'] = Gauge(name='temp2', documentation='Temp 2', unit=temperature_unit)
+    temperature['temp3'] = Gauge(name='temp3', documentation='Temp 3', unit=temperature_unit)
+    temperature['temp4'] = Gauge(name='temp4', documentation='Temp 4', unit=temperature_unit)
+    temperature['temp5'] = Gauge(name='temp5', documentation='Temp 5', unit=temperature_unit)
+    temperature['temp6'] = Gauge(name='temp6', documentation='Temp 6', unit=temperature_unit)
+    temperature['temp7'] = Gauge(name='temp7', documentation='Temp 7', unit=temperature_unit)
+    temperature['temp8'] = Gauge(name='temp8', documentation='Temp 8', unit=temperature_unit)
 
     generic={}
-    generic['humidity'] = Gauge(name='humidity', documentation='humidity', unit='percent')
-    generic['humidityin'] = Gauge(name='humidityin', documentation='humidityin', unit='percent')
-    generic['winddir'] = Gauge(name='winddir', documentation='winddir', unit='degree')
-    generic['uv'] = Gauge(name='uv', documentation='uv')
-    generic['solarradiation'] = Gauge(name='solarradiation', documentation='Solar radiation', unit='klux')
+    generic['humidity'] = Gauge(name='humidity', documentation='Outdoor humidity', unit='percent')
+    generic['humidityin'] = Gauge(name='humidityin', documentation='Indoor humidity', unit='percent')
+    generic['winddir'] = Gauge(name='winddir', documentation='Wind direction', unit='degree')
+    generic['uv'] = Gauge(name='uv', documentation='UV index')
+    
+    irradiance={}
+    irradiance['solarradiation'] = Gauge(name='solarradiation', documentation='Solar irradiance', unit='wm2')
 
     pressure={}
-    pressure['baromrelin'] = Gauge(name='baromrelin', documentation='baromrelin', unit='in')
-    pressure['baromabsin'] = Gauge(name='baromabsin', documentation='baromabsin', unit='in')
-    pressure['baromrelhpa'] = Gauge(name='baromrelhpa', documentation='baromrelhpa', unit='hpa')
-    pressure['baromabshpa'] = Gauge(name='baromabshpa', documentation='baromabshpa', unit='hpa')
+    pressure['baromrel'] = Gauge(name='baromrel', documentation='Relative barometer', unit=pressure_unit)
+    pressure['baromabs'] = Gauge(name='baromabs', documentation='Absolute barometer', unit=pressure_unit)
 
     wind={}
-    wind['windspeedmph'] = Gauge(name='windspeedmph', documentation='windspeedmph', unit='mph')
-    wind['windgustmph'] = Gauge(name='windgustmph', documentation='windgustmph', unit='mph')
-    wind['maxdailygust'] = Gauge(name='maxdailygustmph', documentation='maxdailygustmph', unit='mph')
-    wind['windspeedkmh'] = Gauge(name='windspeedkmh', documentation='windspeedkmh', unit='kmh')
-    wind['windgustkmh'] = Gauge(name='windgustkmh', documentation='windgustkmh', unit='kmh')
-    wind['maxdailygustkmh'] = Gauge(name='maxdailygustkmh', documentation='maxdailygustkmh', unit='kmh')
+    wind['windspeed'] = Gauge(name='windspeed', documentation='Wind speed', unit=wind_unit)
+    wind['windgust'] = Gauge(name='windgust', documentation='Wind gust', unit=wind_unit)
+    wind['maxdailygust'] = Gauge(name='maxdailygust', documentation='Max daily gust', unit=wind_unit)
 
     rain={}
-    rain['rainratein'] = Gauge(name='rainratein', documentation='rainratein', unit='in')
-    rain['eventrainin'] = Gauge(name='eventrainin', documentation='eventrainin', unit='in')
-    rain['hourlyrainin'] = Gauge(name='hourlyrainin', documentation='hourlyrainin', unit='in')
-    rain['dailyrainin'] = Gauge(name='dailyrainin', documentation='dailyrainin', unit='in')
-    rain['weeklyrainin'] = Gauge(name='weeklyrainin', documentation='weeklyrainin', unit='in')
-    rain['monthlyrainin'] = Gauge(name='monthlyrainin', documentation='monthlyrainin', unit='in')
-    rain['yearlyrainin'] = Gauge(name='yearlyrainin', documentation='yearlyrainin', unit='in')
-    rain['totalrainin'] = Gauge(name='totalrainin', documentation='totalrainin', unit='in')
-    rain['rainratemm'] = Gauge(name='rainratemm', documentation='rainratemm', unit='mm')
-    rain['eventrainmm'] = Gauge(name='eventrainmm', documentation='eventrainmm', unit='mm')
-    rain['hourlyrainmm'] = Gauge(name='hourlyrainmm', documentation='hourlyrainmm', unit='mm')
-    rain['dailyrainmm'] = Gauge(name='dailyrainmm', documentation='dailyrainmm', unit='mm')
-    rain['weeklyrainmm'] = Gauge(name='weeklyrainmm', documentation='weeklyrainmm', unit='mm')
-    rain['monthlyrainmm'] = Gauge(name='monthlyrainmm', documentation='monthlyrainmm', unit='mm')
-    rain['yearlyrainmm'] = Gauge(name='yearlyrainmm', documentation='yearlyrainmm', unit='mm')
-    rain['totalrainmm'] = Gauge(name='totalrainmm', documentation='totalrainmm', unit='mm')
+    rain['rainrate'] = Gauge(name='rainrate', documentation='Rainfall rate', unit=rain_unit)
+    rain['eventrain'] = Gauge(name='eventrain', documentation='Event rainfall', unit=rain_unit)
+    rain['hourlyrain'] = Gauge(name='hourlyrain', documentation='Hourly rainfall', unit=rain_unit)
+    rain['dailyrain'] = Gauge(name='dailyrain', documentation='Daily rainfall', unit=rain_unit)
+    rain['weeklyrain'] = Gauge(name='weeklyrain', documentation='Weekly rainfall', unit=rain_unit)
+    rain['monthlyrain'] = Gauge(name='monthlyrain', documentation='Monthly rainfall', unit=rain_unit)
+    rain['yearlyrain'] = Gauge(name='yearlyrain', documentation='Yearly rainfall', unit=rain_unit)
+    rain['totalrain'] = Gauge(name='totalrain', documentation='Total rainfall', unit=rain_unit)
 
     # Add prometheus wsgi middleware to route /metrics requests
     app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
