ded910919e3697276670afd58deb6217654e3ff2
Jonathan Gazeley <me@jonathangazeley.com>
2023-06-21 10:39:33 +0100

    Revert "Parse the form data manually"
    This reverts commit c70d8e868e3cd38a4166590cba175eb45c817d6c.

--- ecowitt_exporter.py
+++ ecowitt_exporter.py
@@ -31,10 +31,22 @@ def version():
 @app.route('/report', methods=['POST'])
 def logEcowitt():
 
+    # This must be accessed before anything else
+    body = request.get_data(as_text=True)
 
     # Retrieve the POST body
-    for field in request.get_data(as_text=True).split('&'):
-        [key, value] = field.split('=')
+    data = request.form
+
+    if debug:
+        print('HEADERS')
+        print(request.headers)
+        print('BODY')
+        print(body)
+        print('FORM DATA')
+        print(data)
+
+    for key in data:
+        value = data[key]
 
         if debug:
             print(f"  Received raw value {key}: {value}")
